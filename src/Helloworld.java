b32d31ef709b3bfeb2c721e8dc1eabd6034bb9f1
Kun Hong <chingu100@gmail.com>
2018-03-03 12:15:17 +0900

    add static value

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,5 +1,7 @@
 public class Helloworld {
+    public static int value = 100;
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(value);
     }
 }
